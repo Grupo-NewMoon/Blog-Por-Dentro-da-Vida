2006cce208f8f6b7c812300fb90851b7dc66422d
lulaluano <luanferreira.ifsul@gmail.com>
2021-03-04 16:00:26 -0300

    fonte do footer trocada de 16px para 14px

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
     return (
         <footer className="container-fluid p-0">
             <div className="container d-flex justify-content-between align-items-center h-100">
-                <a href="#" className="text-white text-decoration-none">Conheça mais sobre a Logosofia</a>
-                <span className="text-white">Copyright © 2021 – Por Dentro da Vida</span>
+                <a href="#" className="text-white text-decoration-none fs-14">Conheça mais sobre a Logosofia</a>
+                <span className="text-white fs-14">Copyright © 2021 – Por Dentro da Vida</span>
             </div>
         </footer>
     )
