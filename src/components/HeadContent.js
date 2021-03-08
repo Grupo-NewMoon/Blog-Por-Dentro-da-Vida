b13286d9d79e25738b8c1185dde1cd20a48324e0
lulaluano <luanferreira.ifsul@gmail.com>
2021-03-08 11:11:40 -0300

    removidos imports desnecessários (css, fonts)

--- src/components/HeadContent.js
+++ src/components/HeadContent.js
@@ -9,12 +9,10 @@ export default function HeadContent(props) {
             <link rel="apple-touch-icon" href="favicon.png" />
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css"/>
             <link rel="stylesheet" href="/css/main.css" />
-            <link rel="stylesheet" href="/css/fonts.min.css" />
-            <link rel="stylesheet" href="/css/solid.min.css" />
-            <link rel="stylesheet" href="https://use.typekit.net/din3oeo.css" />
             <link rel="preconnect" href="https://fonts.gstatic.com" />
-            <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700;900&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600&display=swap" rel="stylesheet" />
             <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet" />
+            <link rel="stylesheet" href="https://use.typekit.net/din3oeo.css" />
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" />
             <script src="https://kit.fontawesome.com/862ff8d067.js" crossOrigin="anonymous" />
         </Head>
