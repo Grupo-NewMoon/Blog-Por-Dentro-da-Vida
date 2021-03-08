aa5bf7606e01f57d4adf054a3d614e1cf72f66da
lulaluano <luanferreira.ifsul@gmail.com>
2021-03-08 10:44:15 -0300

    fundo consertado na página de post

--- src/components/Title.js
+++ src/components/Title.js
@@ -3,7 +3,8 @@ import Image from 'next/image'
 
 export default function Title() {
     return (
-        <div className="container-fluid p-0 fundo">
+        <div className="container-fluid p-0 position-relative">
+            <div className="fundo-post" />
             <div className="container d-flex justify-content-center flex-column">
                 <h1 className="post-heading">O trem da vida não para pra você subir</h1>
                 <div className="flex-center" id="post-date-row">
--- src/components/Ultimas.js
+++ src/components/Ultimas.js
@@ -4,25 +4,28 @@ import Publicacao from "./Publicacao";
 
 export default function Ultimas() {
     return (
-        <div className="container" id="ultimas-publicacoes">
-            <div className="text-center">
-                <SubLinhado textColor="sec" textSize="56" linePosition="center" lineColor="main">
-                    Últimas Publicações
-                </SubLinhado>
-            </div>
+        <div className="container-fluid p-0 position-relative">
+            <div className="fundo-post-2" />
+            <div className="container" id="ultimas-publicacoes">
+                <div className="text-center">
+                    <SubLinhado textColor="sec" textSize="56" linePosition="center" lineColor="main">
+                        Últimas Publicações
+                    </SubLinhado>
+                </div>
 
-            <div className="row mx-auto justify-content-between" id="ultimas">
-                <div className="col-1" />
-                <Publicacao dia="21" mes="setembro" ano="19" img="1">
-                    Pensamentos Tóxicos
-                </Publicacao>
-                <Publicacao dia="17" mes="setembro" ano="19" img="2">
-                    Exemplo do pai
-                </Publicacao>
-                <Publicacao dia="20" mes="agosto" ano="19" img="3">
-                    O paralisador abraço do polvo
-                </Publicacao>
-                <div className="col-1" />
+                <div className="row mx-auto justify-content-between" id="ultimas">
+                    <div className="col-1" />
+                    <Publicacao dia="21" mes="setembro" ano="19" img="1">
+                        Pensamentos Tóxicos
+                    </Publicacao>
+                    <Publicacao dia="17" mes="setembro" ano="19" img="2">
+                        Exemplo do pai
+                    </Publicacao>
+                    <Publicacao dia="20" mes="agosto" ano="19" img="3">
+                        O paralisador abraço do polvo
+                    </Publicacao>
+                    <div className="col-1" />
+                </div>
             </div>
         </div>
     )
